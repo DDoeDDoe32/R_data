58e96ff46cf0df889cf9e9d8981fed28080a7f1f
DDoeDDoe32 <42180081+DDoeDDoe32@users.noreply.github.com>
2018-10-06 12:54:15 +0900

    day1006

--- 1006/텍스트마이닝.R
+++ 1006/텍스트마이닝.R
@@ -4,3 +4,143 @@ install.packages("KoNLP")
 
 library(KoNLP)
 useNIADic()
+
+txt <- readLines("./data/14/hiphop.txt")
+head(txt)
+
+library(stringr)
+
+# 특수문제 제거
+txt <- str_replace_all(txt, "\\W", " ")
+
+extractNoun("대한민국의 영토는 한반도와 그 부속도서로 한다")
+
+# 가사에서 명사추출
+nouns <- extractNoun(txt)
+
+# 추출한 명사 list를 문자열 벡터로 변환, 단어별 빈도표 생성
+wordcount <- table(unlist(nouns))
+
+# 데이터 프레임으로 변환
+df_word <- as.data.frame(wordcount, stringsAsFactors = F)
+head(df_word)
+# 변수명 수정
+df_word <- rename(df_word,
+                  word = Var1,
+                  freq = Freq)
+head(df_word)
+
+# 두 글자 이상 단어 추출
+df_word <- filter(df_word, nchar(word) >= 2)
+
+#빈도순으로 정렬한 후 상위 20개 단어를 추출한다.
+top_20 <- df_word %>%
+  arrange(desc(freq)) %>%
+  head(20)
+
+top_20
+
+# 패키지 설치
+install.packages("wordcloud")
+
+# 패키지 로드
+library(wordcloud)
+library(RColorBrewer)
+
+pal <- brewer.pal(8,"Dark2")  # Dark2 색상 목록에서 8개 색상 추출
+
+wordcloud(words = df_word$word,  # 단어
+          freq = df_word$freq,   # 빈도
+          min.freq = 2,          # 최소 단어 빈도
+          max.words = 200,       # 표현 단어 수
+          random.order = F,      # 고빈도 단어 중앙 배치
+          rot.per = .1,          # 회전 단어 비율
+          scale = c(4, 0.3),     # 단어 크기 범위
+          colors = pal)          # 색깔 목록
+
+pal <- brewer.pal(9,"Blues")[5:9]  # 색상 목록 생성
+
+wordcloud(words = df_word$word,    # 단어
+          freq = df_word$freq,     # 빈도
+          min.freq = 2,            # 최소 단어 빈도
+          max.words = 200,         # 표현 단어 수
+          random.order = F,        # 고빈도 단어 중앙 배치
+          rot.per = .1,            # 회전 단어 비율
+          scale = c(4, 0.3),       # 단어 크기 범위
+          colors = pal)            # 색상 목록
+
+
+# 데이터 로드
+twitter <- read.csv("./data/14/twitter.csv",
+                    header = T,
+                    stringsAsFactors = F,
+                    fileEncoding = "UTF-8")
+
+# 변수명 수정
+twitter <- rename(twitter,
+                  no = 번호,
+                  id = 계정이름,
+                  date = 작성일,
+                  tw = 내용)
+
+# 특수문자 제거
+twitter$tw <- str_replace_all(twitter$tw, "\\W", " ")
+
+head(twitter$tw)
+
+
+# 트윗에서 명사추출
+nouns <- extractNoun(twitter$tw)
+
+# 추출한 명사 list를 문자열 벡터로 변환, 단어별 빈도표 생성
+wordcount <- table(unlist(nouns))
+
+# 데이터 프레임으로 변환
+df_word <- as.data.frame(wordcount, stringsAsFactors = F)
+
+# 변수명 수정
+df_word <- rename(df_word,
+                  word = Var1,
+                  freq = Freq)
+
+
+# 두 글자 이상 단어만 추출
+df_word <- filter(df_word, nchar(word) >= 2)
+
+# 상위 20개 추출
+top20 <- df_word %>%
+  arrange(desc(freq)) %>%
+  head(20)
+
+top20
+
+library(ggplot2)
+
+order <- arrange(top20, freq)$word               # 빈도 순서 변수 생성
+
+ggplot(data = top20, aes(x = word, y = freq)) +  
+  ylim(0, 2500) +
+  geom_col() + 
+  coord_flip() +
+  scale_x_discrete(limit = order) +              # 빈도 순서 변수 기준 막대 정렬
+  geom_text(aes(label = freq), hjust = -0.3)     # 빈도 표시
+
+pal <- brewer.pal(8,"Dark2")       # 색상 목록 생성
+
+wordcloud(words = df_word$word,    # 단어
+          freq = df_word$freq,     # 빈도
+          min.freq = 10,           # 최소 단어 빈도
+          max.words = 200,         # 표현 단어 수
+          random.order = F,        # 고빈도 단어 중앙 배치(출력되는 순서를 임의로 지정)
+          rot.per = .1,            # 회전 단어 비율
+          scale = c(6, 0.2),       # 단어 크기 범위
+          colors = pal)            # 색상 목록
+
+# 색상 정보
+brewer.pal.info
+
+par(mfrow=c(1, 3))
+display.brewer.all(type="div")
+display.brewer.all(type="seq")
+display.brewer.all(type="qual")
+
